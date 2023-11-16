09c7eb7c3ad5af2c2c32494ac2591814f49d04d1
Rashid <rashidgaitov@gmail.com>
2023-11-16 09:08:44 +0300

    Initial Commit

---
+++ DeepFocus/DeepFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepFocusApp.swift
+//  DeepFocus
+//
+//  Created by Rashid Gaitov on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
